1ec5243e972d6c6edf6c60aed89a0e7955282038
elfamonali <elfamonali75@gmail.com>
2026-08-18 20:05:27 +0530

    Added Entity Models, Data Context and Migrations

---
+++ App/CodeReviewer.Api/Models/Submission.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeReviewer.Api.Models
+{
+    public class Submission
+    {
+        [Key]
+        public int Id {get;set;}
+
+        [Required]
+        public int StudentId {get;set;}
+
+        [Required]
+        public int AssignmentId {get;set;}
+
+        [Required]
+        public string CodeText {get;set;} = string.Empty;
+
+        public DateTime SubmissionDate {get;set;} = DateTime.UtcNow;
+
+        [ForeignKey("StudentId")]
+        public User? Student {get;set;}
+
+        [ForeignKey("AssignmentId")]
+        public Assignment? Assignment {get;set;}
+
+        public string Language { get; set; } = "csharp";
+        public int? Grade { get; set; }
+        public string? AiFeedback { get; set; }
+    }
+}
---
+++ App/CodeReviewer.Api/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeReviewer.Api.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Role {get;set;} = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string Name {get;set;}=string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email {get;set;} = string.Empty;
+
+        [Required]
+        public string Password {get;set;} = string.Empty;
+    }
+}
