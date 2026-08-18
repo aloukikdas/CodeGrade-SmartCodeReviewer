a46d3ec557e0fc6cd9b45f63be40046c545a3e7b
Sanjana Krishnan <skmk0503@gmail.com>
2026-08-18 20:35:41 +0530

    API: Implemented DTOs for secure data transfer

---
+++ App/CodeReviewer.Api/DTOs/ClassroomDtos.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeReviewer.Api.DTOs
+{
+    public class CreateClassroomDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public int TeacherId { get; set; }
+    }
+
+    public class EnrollDto
+    {
+        public string JoinCode { get; set; } = string.Empty;
+        public int StudentId { get; set; }
+    }
+
+    public class CreateAssignmentDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int ClassroomId { get; set; }
+        public string AllowedLanguage { get; set; } = "Any";
+    }
+}
---
+++ App/CodeReviewer.Api/DTOs/GradingPayload.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeReviewer.Api.Models
+{
+    public class GradingPayload
+    {
+        public int StudentId { get; set; }
+        public int AssignmentId { get; set; }
+        public int Grade { get; set; }
+        public string? AiFeedback { get; set; }
+    }
+}
